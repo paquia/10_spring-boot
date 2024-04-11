f3ee1aebe12217bdfa942d2578bc82a5e420ba5e
junhyeok <jhsh9912@gmail.com>
2024-04-11 14:45:02 +0900

    [commit]

---
+++ src/test/java/com/ohgiraffers/autoconfiguration/Chap01AutoConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.autoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01AutoConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
